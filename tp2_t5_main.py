93eef010306118ad1d5cb434dd8802d5ece923a2
Fernando Davila <a91403@ualg.pt>
2026-03-14 14:18:18 +0000

    Added logic in show_menu()

--- tp2_t5_main.py
+++ tp2_t5_main.py
@@ -7,4 +7,15 @@ def start_game():
     pass
 
 def show_menu():
-    pass
+    while True:
+        print("\n---GOMOKU---")
+        print("1) Start Game")
+        print("2) Exit")
+
+        option= input("Choose an option: ")
+        if option== "1":
+            start_game()
+        elif option== "2":
+            print("Bye")
+        else:
+            print("Invalid option")
